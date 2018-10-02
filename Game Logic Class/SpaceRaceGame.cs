0a7400abe95e47165289cf1bb1efa48e61f2ecfa
nick22985 <nicholasdhonda@gmail.com>
2018-10-02 17:38:58 +1000

    Initial Upload

---
+++ Game Logic Class/SpaceRaceGame.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing;
+using System.ComponentModel;
+using Object_Classes;
+
+
+namespace Game_Logic_Class
+{
+    public static class SpaceRaceGame
+    {
+        // Minimum and maximum number of players.
+        public const int MIN_PLAYERS = 2;
+        public const int MAX_PLAYERS = 6;
+
+        private static int numberOfPlayers = 2;  //default value for test purposes only 
+        public static int NumberOfPlayers
+        {
+            get
+            {
+                return numberOfPlayers;
+            }
+            set
+            {
+                numberOfPlayers = value;
+            }
+        }
+
+        public static string[] names = { "One", "Two", "Three", "Four", "Five", "Six" };  // default values
+
+        // Only used in Part B - GUI Implementation, the colours of each player's token
+        private static Brush[] playerTokenColours = new Brush[MAX_PLAYERS] { Brushes.Yellow, Brushes.Red,
+                                                                       Brushes.Orange, Brushes.White,
+                                                                      Brushes.Green, Brushes.DarkViolet};
+        /// <summary>
+        /// A BindingList is like an array which grows as elements are added to it.
+        /// </summary>
+        private static BindingList<Player> players = new BindingList<Player>();
+        public static BindingList<Player> Players
+        {
+            get
+            {
+                return players;
+            }
+        }
+
+        // The pair of die
+        private static Die die1 = new Die(), die2 = new Die();
+
+
+        /// <summary>
+        /// Set up the conditions for this game as well as
+        ///   creating the required number of players, adding each player 
+        ///   to the Binding List and initialize the player's instance variables
+        ///   except for playerTokenColour and playerTokenImage in Console implementation.
+        ///   
+        ///     
+        /// Pre:  none
+        /// Post:  required number of players have been initialsed for start of a game.
+        /// </summary>
+        public static void SetUpPlayers()
+        {
+            //Creates speicfied players for game
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players.Add(new Player(names[i]));
+            }
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players[i].RocketFuel = Player.INITIAL_FUEL_AMOUNT;
+            }
+            // for number of players
+            //      create a new player object
+            //      initialize player's instance variables for start of a game
+            //      add player to the binding list
+
+        }
+
+        /// <summary>
+        ///  Plays one round of a game
+        /// </summary>
+        public static void PlayOneRound()
+        {
+            /*int roundCounter = 1;
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                int totalDieNumber = 0;
+                int type = -1;
+                Die dieResult1 = new Die(4);
+                Die dieResult2 = new Die(4);
+                int dieface1 = dieResult1.Roll();
+                int dieface2 = dieResult2.Roll();
+                totalDieNumber = dieface1 + dieface2;
+                players[i].Position = totalDieNumber + players[i].Position;
+                type = Board.whatTypeOfSquare(players[i].Position);
+                dieResult1.Reset();
+                dieResult2.Reset();
+            }
+            roundCounter++;*/
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players[i].Play(die1, die2);
+            }
+
+        }
+
+    }//end SnakesAndLadders
+}
